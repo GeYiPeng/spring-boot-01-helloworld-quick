ee4e641121e43c4e51c0e90991bfe7d5e9db3272
GeYiPeng <49312953+GeYiPeng@users.noreply.github.com>
2021-04-20 15:40:14 +0800

    Update HelloWorldController.java

--- src/main/java/com/atguigu/springboot/controller/HelloWorldController.java
+++ src/main/java/com/atguigu/springboot/controller/HelloWorldController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world gyp";
     }
 }
